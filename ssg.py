4d14eed8704ce513446cb757d048dc32c330f06a
David Okeke <davidokeke.c@gmail.com>
2022-05-26 13:17:13 +0100

    Part_2, final_submission

--- ssg.py
+++ ssg.py
@@ -2,10 +2,10 @@ import typer
 from ssg.site import Site
 
 
-def main(source="contents", dest="dist"):
+def main(source="content", dest="dist"):
     config = {"source": source, "dest": dest}
 
     test_site = Site(**config).build()
 
 
-typer.run(main())
+typer.run(main)
