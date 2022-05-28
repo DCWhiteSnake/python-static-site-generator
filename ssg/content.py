35b199a655f56c513d38d96b205f3c0bc1f004ce
David Okeke <davidokeke.c@gmail.com>
2022-05-28 21:41:43 +0100

    Part_4, final submission .

--- ssg/content.py
+++ ssg/content.py
@@ -21,6 +21,7 @@ class Content(Mapping):
     def body(self):
         return self.data["content"]
 
+
     @property
     def type(self):
         return  self.data["type"] if "type" in self.data else None
